0c02787ffb9316469a8fc2e100554205f50e1ffc
khadija iftikhar <161138007+khadijaiftikhar7@users.noreply.github.com>
2025-11-18 22:25:27 +0500

    Refactor Graph class to use Node pointers

--- Graph.h
+++ Graph.h
@@ -4,17 +4,19 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include "Node.h"
 #include <vector>
 #include <utility>
 #include <string>
 
 class Graph {
-    private:
+    public:
     //total number of nodes in a graph
     int numberOfNodes;
+    static int idxCounter;
 
-    //maps each node's index to its corresponding location name
-    std::vector<std::string> locationNames;
+    //stores all the nodes of the graph
+    std::vector<Node*> Nodes;
 
     //std::pair<int, double> means there's an edge from a given node to its neighbouring node of value int with edge cost double
     //std::vector<std::pair<int, double>> is a vector that stores ALL the neighbouring nodes of a GIVEN node in (value, cost) pairs
@@ -22,27 +24,33 @@ class Graph {
     std::vector<std::vector<std::pair<int, double>>> neighboursList;
 
 
-
-    public:
     //CONSTRUCTORS
     //A constructor to initialize our graph with a specified number of nodes
     Graph(int numberOfNodes);
+
     //initializes an empty graph with NO nodes
     Graph();
 
     //METHODS
     //Creates and adds a node with given location to locationNames list. Creates empty vector for it in the neighboursList. Added node is not connected to the graph yet
     void addNode(const std::string& name);
+
     //Connects two nodes with an edge of cost = weight
-    void addEdge(int sourceNode, int destinationNode, double weight);
+    void addEdge(const std::string& sourceNode, const std::string& destinationNode, double weight);
+
     //removes a node from the graph, also removing any edges between it and its neighbours
-    void removeNode(int index);
+    void removeNode(const std::string& name);
+
     //Only removes connection between two nodes, nodes remain as they are
-    void removeEdge(int sourceNode, int destinationNode);
+    void removeEdge(std::string sourceNode, std::string destinationNode);
+
     //return index of a node with the given location name
-    int getIndex(const std::string& location);
+    int getIndex(const std::string& name);
+
     //return name of location/place of a node with the given index
     std::string getLocation(int index);
+
+    int getNumberOfNodes();
 
 };
 
