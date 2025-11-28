8f01e57ab2b1a9a1893ad2d0b66bb4b27689f216
ArbazNadeemKhan <arbaznk5426@gmail.com>
2025-11-28 18:31:10 +0500

    Adding Updater.h

---
+++ Updater.h
@@ -0,0 +1,41 @@
+#ifndef UPDATER_H
+#define UPDATER_H
+
+#include "Graph.h"
+#include "PathFinder.h"
+#include <string>
+#include <unordered_set>
+
+enum Weather { CLEAR, RAIN, STORM, FLOOD };
+
+class Updater {
+
+private:
+    Graph* graph;
+    Weather currentWeather;
+    std::unordered_set<std::string> restrictedZones;
+
+public:
+    Updater(Graph* g);
+
+    // WEATHER HANDLING
+    void setWeather(Weather w);
+    void applyWeatherEffects();
+
+    // ROAD / EDGE UPDATE
+    void updateEdgeWeight(const std::string& src, const std::string& dest, double newWeight);
+    void blockRoad(const std::string& src, const std::string& dest);
+    void unblockRoad(const std::string& src, const std::string& dest, double weight);
+
+    // VIP RESTRICTIONS
+    void addRestrictedZone(const std::string& zoneName);
+    void clearRestrictedZones();
+
+    // REAL-TIME RE-ROUTING
+    void reRoute(const std::string& source, const std::string& dest);
+
+    // MENU/UI
+    void updateMenu();
+};
+
+#endif
